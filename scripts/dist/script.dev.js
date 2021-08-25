2b885cf5628f9b7658be7cfcfd52d138ff219f10
Noor Ncho <nrandin@gmail.com>
2021-08-25 16:45:10 +0100

    implement first draft of checking for a win

--- scripts/dist/script.dev.js
+++ scripts/dist/script.dev.js
@@ -11,8 +11,9 @@ var BoardGridArr = ["", "", "", "", "", "", "", "", ""]; //Counter variables
 var turnCounter = 0; //Track player turns
 
 var moveCounter = 0; //Tracks number of moved taken
-//console.log(gameButtons)
+//Win Combos - indexes in the array that have to match to win
 
+var winCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 /***** Eventlistener ("click") for all buttons *****/
 
 gameButtons.forEach(function (button) {
@@ -21,7 +22,9 @@ gameButtons.forEach(function (button) {
 
     var gridLocation = selectedButton.id;
     BoardGridArr[gridLocation] = "Hello";
-    selectedButton.textContent = "Hi Bitch";
+    selectedButton.textContent = "Hi Bitch"; //Check if win combo satisfied
+
+    checkForWin();
   });
 });
 restartButton.addEventListener("click", function () {
@@ -46,4 +49,28 @@ var restartGame = function restartGame() {
   });
   console.log(BoardGridArr);
   console.log(gameButtons.length);
+};
+/**
+ * Check for a wining combination
+ */
+
+
+var checkForWin = function checkForWin() {
+  console.log("Check for winner"); //Iterate through grid array and check for win combinations
+
+  for (var i = 0; i < winCombos.length; i++) {
+    var winCombo = winCombos[i];
+    var a = BoardGridArr[winCombo[0]];
+    var b = BoardGridArr[winCombo[1]];
+    var c = BoardGridArr[winCombo[2]];
+
+    if (a === "" || b === "" || c === "") {
+      continue;
+    }
+
+    if (a === b && b === c) {
+      console.log("Winner");
+      break;
+    }
+  }
 };
--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,26 @@
 //Board Grid buttons
 const gameButtons = document.querySelectorAll(".board_button");;
+
 //Restart buttons
 const restartButton = document.querySelector("#restart");
+
 //Board grid array - grid = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 const BoardGridArr = ["", "", "", "", "", "", "", "", ""];
+
 //Counter variables
 let turnCounter = 0; //Track player turns
 let moveCounter = 0; //Tracks number of moved taken
 
-//console.log(gameButtons)
+//Win Combos - indexes in the array that have to match to win
+const winCombos = [[0, 1, 2], 
+                    [3, 4, 5], 
+                    [6, 7, 8], 
+                    [0, 3, 6], 
+                    [1, 4, 7], 
+                    [2, 5, 8], 
+                    [0, 4, 8], 
+                    [2, 4, 6]];
+
 
 /***** Eventlistener ("click") for all buttons *****/
 
@@ -19,6 +31,9 @@ gameButtons.forEach(button => {
         
         BoardGridArr[gridLocation] = "Hello"
         selectedButton.textContent = "Hi Bitch"
+        
+        //Check if win combo satisfied
+        checkForWin();
     });
 });
 
@@ -45,4 +60,28 @@ const restartGame = () => {
 
     console.log(BoardGridArr);
     console.log(gameButtons.length);
+}
+
+/**
+ * Check for a wining combination
+ */
+const checkForWin = () => {
+    console.log("Check for winner");
+
+    //Iterate through grid array and check for win combinations
+    for(let i = 0; i < winCombos.length; i++){
+        const winCombo = winCombos[i];
+        let a = BoardGridArr[winCombo[0]];
+        let b = BoardGridArr[winCombo[1]];
+        let c = BoardGridArr[winCombo[2]];
+
+        if(a === "" || b === "" || c === ""){
+            continue;
+        }
+        if(a === b && b === c){
+            console.log("Winner");
+            break;
+        }
+        
+    }
 }
