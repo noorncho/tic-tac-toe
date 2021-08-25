32363d073e6189bd9c140dbee70e6ec8f1a1c23a
Noor Ncho <nrandin@gmail.com>
2021-08-25 15:47:20 +0100

    Impletement restart button to clear board

--- scripts/dist/script.dev.js
+++ scripts/dist/script.dev.js
@@ -6,7 +6,12 @@ var gameButtons = document.querySelectorAll(".board_button");
 
 var restartButton = document.querySelector("#restart"); //Board grid array - grid = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
-var BoardGridArr = ["", "", "", "", "", "", "", "", ""]; //console.log(gameButtons)
+var BoardGridArr = ["", "", "", "", "", "", "", "", ""]; //Counter variables
+
+var turnCounter = 0; //Track player turns
+
+var moveCounter = 0; //Tracks number of moved taken
+//console.log(gameButtons)
 
 /***** Eventlistener ("click") for all buttons *****/
 
@@ -16,6 +21,29 @@ gameButtons.forEach(function (button) {
 
     var gridLocation = selectedButton.id;
     BoardGridArr[gridLocation] = "Hello";
-    selectedButton.textContent = "Hi Bitch"; // console.table(BoardGridArr);
+    selectedButton.textContent = "Hi Bitch";
   });
 });
+restartButton.addEventListener("click", function () {
+  restartGame();
+});
+/**
+ * Restart Function 
+ */
+
+var restartGame = function restartGame() {
+  //Reset Counters
+  turnCounter = 0;
+  moveCounter = 0; //Remove all values from array
+
+  for (var i = 0; i < BoardGridArr.length; i++) {
+    BoardGridArr[i] = "";
+  } //Remove text from buttons
+
+
+  gameButtons.forEach(function (button) {
+    return button.textContent = "";
+  });
+  console.log(BoardGridArr);
+  console.log(gameButtons.length);
+};
--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,9 @@ const gameButtons = document.querySelectorAll(".board_button");;
 const restartButton = document.querySelector("#restart");
 //Board grid array - grid = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 const BoardGridArr = ["", "", "", "", "", "", "", "", ""];
+//Counter variables
+let turnCounter = 0; //Track player turns
+let moveCounter = 0; //Tracks number of moved taken
 
 //console.log(gameButtons)
 
@@ -15,7 +18,31 @@ gameButtons.forEach(button => {
         const gridLocation = selectedButton.id;
         
         BoardGridArr[gridLocation] = "Hello"
-        selectedButton.textContent = "Hi Bitch"    
-        // console.table(BoardGridArr);
+        selectedButton.textContent = "Hi Bitch"
     });
 });
+
+restartButton.addEventListener("click", () => {
+    restartGame();
+});
+
+
+/**
+ * Restart Function 
+ */
+const restartGame = () => {
+    //Reset Counters
+    turnCounter = 0;
+    moveCounter = 0;
+
+    //Remove all values from array
+    for(let i = 0; i < BoardGridArr.length; i++){
+        BoardGridArr[i] = "";
+    }
+
+    //Remove text from buttons
+    gameButtons.forEach(button => button.textContent = "");
+
+    console.log(BoardGridArr);
+    console.log(gameButtons.length);
+}
